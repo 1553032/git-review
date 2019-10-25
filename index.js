0842a028a4616eed88e2a568fc8d6e8214c9310a
ThinhNVQ <thinhnvq@evolableasia.vn>
2019-10-25 13:21:56 +0700

    Add more function to Cat

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ dog.introduce();
 
 let tom = new Cat("Tom");
 dog.eat(tom);
+
+let Jerry = new Mouse('Jerry');
+tom.eat(Jerry);
+
+try{
+    tom.eat(dog);
+} catch (error) {
+    console.log(error);
+}
